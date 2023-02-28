3952b3cfffc5092709d46a4a5ab81357389d1d8d
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-28 14:30:09 +0530

    Adds the ApiTags

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { Body, Post } from '@nestjs/common/decorators';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   // dependency injection: which class I need to instantiate for you behind the scenes
