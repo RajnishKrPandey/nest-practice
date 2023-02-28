6e99f556291b4fb45bdbad8462ab3e5a2c4d6413
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-28 14:23:57 +0530

    Congigure the Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest API')
+    .setDescription('The Description of the API')
+    .setVersion('1.0')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    SwaggerModule.setup('/',app, document);
+
   await app.listen(3000);
 }
 bootstrap();
