c6477d0a9f2bb512ceede444051dfcaca25eeb40
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-28 14:00:50 +0530

    Create user.entity.ts file

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+
+// This is the class which represents the shape of the user objects In future if get into DB connections this entity class also represents database tables. Entities usually map one to one with database tables.
+export class User {
+    id: number;
+
+    name: string;
+}
