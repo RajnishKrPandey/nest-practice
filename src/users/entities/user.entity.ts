5d87b91aa9c270f4aba9529ad2e9a4f620bafd10
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-28 15:29:32 +0530

    APIs responds with status code

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,12 @@
 /* eslint-disable prettier/prettier */
 
+import { ApiProperty } from "@nestjs/swagger";
+
 // This is the class which represents the shape of the user objects In future if get into DB connections this entity class also represents database tables. Entities usually map one to one with database tables.
 export class User {
+    @ApiProperty()
     id: number;
-
+    
+    @ApiProperty()
     name?: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
+/* eslint-disable prettier/prettier */
 import { Controller, Get, Param } from '@nestjs/common';
 import { Body, Post } from '@nestjs/common/decorators';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
@@ -11,16 +12,18 @@ export class UsersController {
   // dependency injection: which class I need to instantiate for you behind the scenes
   constructor(private usersService: UsersService) {}
 
+  @ApiOkResponse({type: User, isArray: true})
   @Get()
   getUsers(): User[] {
     return this.usersService.findAll();
   }
-
+  
+  @ApiOkResponse({type: User})
   @Get(':id')
   getUserById(@Param('id') id: string): User {
     return this.usersService.findById(Number(id));
   }
-
+  @ApiCreatedResponse({type: User})
   @Post()
   createUser(@Body() body: CreateUserDto): User {
     return this.usersService.createUser(body);
