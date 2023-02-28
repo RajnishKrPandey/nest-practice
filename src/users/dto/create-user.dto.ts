615a608fe4f0d8c4982277124e3e8564b08176d0
Rajnish Kumar Pandey <piyushwinner1997@gmail.com>
2023-02-28 15:09:16 +0530

    Adds decoraters for required field

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,12 @@
 /* eslint-disable prettier/prettier */
-//DTO: Data Transfer object is just a schema representation for of whatever object you want to use intermediary way to transfer information.
 
+import { ApiProperty } from "@nestjs/swagger";
+
+//DTO: Data Transfer object is just a schema representation for of whatever object you want to use intermediary way to transfer information.
 export class CreateUserDto {
+    @ApiProperty()
     name: string;
+
+    @ApiProperty({required: false})
+    age?: number;
 }
